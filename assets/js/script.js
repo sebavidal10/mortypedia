a28160faf93e82fbcd656747114eb85052a3ec25
Sebastian Vidal Aedo <sebastian@intercode.cl>
2022-02-03 17:32:23 -0300

    add loading gif

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,24 +22,36 @@ const getCharacter = async (name, currentPage = 1) => {
 const showData = (data) => {
   for (let char of data) {
     let div_char = document.createElement('div');
-    div_char.className = 'col-6 col-md-2 p-3';
+    div_char.className = 'col-6 col-md-2 p-3 text-white';
 
-    let name_char = document.createElement('h5');
-    name_char.textContent = char.name;
-    div_char.appendChild(name_char);
+    let p_name = document.createElement('p');
+    p_name.className = 'mb-2';
+    if (char.name.length > 16) {
+      p_name.textContent = char.name.substring(0, 16) + '...';
+      p_name.setAttribute('data-toggle', `tooltip`);
+      p_name.setAttribute('data-placement', `bottom`);
+      p_name.setAttribute('title', `${char.name}`);
+    } else {
+      p_name.textContent = char.name;
+    }
+    div_char.appendChild(p_name);
 
     let img_char = document.createElement('img');
     img_char.className =
-      'img-fluid mini-img shadow rounded border border-light';
+      'img-fluid mini-img shadow rounded border border-green-rm';
     img_char.src = char.image;
     div_char.appendChild(img_char);
 
     let p_char = document.createElement('p');
+    p_char.className = 'text-right';
     p_char.textContent = char.status;
     div_char.appendChild(p_char);
 
     $('#files').append(div_char);
   }
+  $('[data-toggle="tooltip"]').tooltip();
+  $('#loading').addClass('d-none');
+  $('#files').removeClass('d-none');
 };
 
 const getThe = async (who) => {
@@ -54,25 +66,16 @@ const getThe = async (who) => {
 };
 
 $(document).ready(function () {
-  [
-    'Rick',
-    'Morty',
-    'Summer',
-    'Beth',
-    'Jerry',
-    'Squanch',
-    'Birdperson',
-    'Tammy',
-  ].forEach((name) => {
+  ['Rick', 'Morty', 'Summer', 'Beth', 'Jerry'].forEach((name) => {
     let btn = document.createElement('button');
-    btn.className = `btn bg-black p-2 pl-3 text-white mx-2 mb-3 rounded-pill`;
+    btn.className = `btn bg-black p-2 pl-3 text-light mx-2 mb-3 rounded-pill`;
     btn.setAttribute('data-name', `btn-get-${name}`);
     btn.innerHTML = `${name}!`;
 
     let img = document.createElement('img');
     img.src = `./assets/img/${name}.gif`;
     img.alt = `${name} gif`;
-    img.className = 'gif-banner rounded-circle ml-2';
+    img.className = 'gif-banner rounded-circle ml-2 border border-light';
     img.setAttribute('data-name', `btn-get-${name}`);
     btn.appendChild(img);
 
@@ -80,8 +83,9 @@ $(document).ready(function () {
   });
 
   $('#buttons').on('click', 'button', function (e) {
+    $('#files').addClass('d-none');
+    $('#loading').removeClass('d-none');
     let who = e.target.getAttribute('data-name').split('-')[2];
-
     getThe(who);
   });
 });
